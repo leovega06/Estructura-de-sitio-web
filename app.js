bf492f93d4e52969f4d58a05c6b3457ac2b95170
leovega06 <leoo_0690@hotmail.com>
2021-06-28 01:27:42 -0300

    trabajo integrador

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const path = require ("path");
+const port = 3030;
+
+const app = express();
+
+app.use(express.static("public"))
+app.get("/",(req,res)=> res.sendFile(path.join(__dirname,"views","home.html")))
+
+app.listen(port,() => console.log(`server running in http://localhost:${port}`))
