520fcceb958cf1142f72f2a40023287bd5056ccb
leovega06 <leoo_0690@hotmail.com>
2021-08-09 20:44:40 -0300

    Login y register

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const port = 3030;
 const app = express();
 
 app.use(express.static("public"))
+
 app.get("/",(req,res)=> res.sendFile(path.join(__dirname,"views","home.html")))
+app.get("/register",(req,res)=> res.sendFile(path.join(__dirname,"views","register.html")))
+app.get("/login",(req,res)=> res.sendFile(path.join(__dirname,"views","login.html")))
 
 app.listen(port,() => console.log(`server running in http://localhost:${port}`))
